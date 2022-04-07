ef1df12f7362340a8d8f313837e382f063cd13a1
Vladimir <newzavod@gmail.com>
2022-04-07 11:04:51 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ const startBrainGame = (rulesOfGame, gameCheck) => {
     console.log(`Question: ${getQuestion}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
-      console.log(typeof correctAnswer, correctAnswer); // !!!
       console.log(`${userAnswer} is wrong answer ; (.Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
