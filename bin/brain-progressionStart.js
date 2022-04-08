b984a81fda412db92f6cfdfbd2c8f20e2560aea9
Vladimir <newzavod@gmail.com>
2022-04-08 06:09:29 +0300

    fix brain-progression.js

--- bin/brain-progressionStart.js
+++ bin/brain-progressionStart.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import startBrainGame from '../src/games/brain-progression.js';
+import brainProgressionGame from '../src/games/brain-progression.js';
 
-startBrainGame();
+brainProgressionGame();
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,41 +1,33 @@
-import readlineSync from 'readline-sync';
 import _ from 'lodash';
+import startBrainGame from '../index.js';
 
-const startBrainGame = () => {
-  const rulesOfGame = 'What number is missing in the progression?';
-  const rounds = 3;
-  const textError = 'is wrong answer ; (.Correct answer was ';
-  const textErrorLine2 = '\n Let\'s try again,';
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${userName}!`);
-  console.log(rulesOfGame);
-  for (let i = 0; i < rounds; i += 1) {
-    let firstElementOfProgression = _.random(1, 100);
-    const stepProgression = _.random(1, 10);
-    const lengthProgression = _.random(5, 10);
-    const missingIndexProgression = _.random(1, (lengthProgression - 1));
-    const numbersProgression = [];
-    const replaceMissingElement = '..';
+// import readlineSync from 'readline-sync';
+// import _ from 'lodash';
+const rulesOfGame = 'What number is missing in the progression?';
 
-    for (let n = 1; n <= lengthProgression; n += 1) {
-      firstElementOfProgression += stepProgression;
-      numbersProgression.push(firstElementOfProgression);
-    }
-
-    const newArr = numbersProgression.splice(missingIndexProgression, 1, replaceMissingElement);
-    console.log(newArr); // missing element
-    const arrayToString = numbersProgression.toString();
-    console.log(`Question: ${arrayToString}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (Number(userAnswer) === Number(newArr)) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' ${textError} '${newArr}' ${textErrorLine2} ${userName}!`);
-      return;
-    }
+const gameCheck = () => {
+  let firstElementOfProgression = _.random(1, 100);
+  const stepProgression = _.random(1, 10);
+  const lengthProgression = _.random(5, 10);
+  const missingIndexProgression = _.random(1, (lengthProgression - 1));
+  const numbersProgression = [];
+  const replaceMissingElement = '..';
+  for (let n = 1; n <= lengthProgression; n += 1) {
+    firstElementOfProgression += stepProgression;
+    numbersProgression.push(firstElementOfProgression);
   }
-  console.log(`Congratulations, ${userName}!`);
+
+  const newArr = numbersProgression.splice(missingIndexProgression, 1, replaceMissingElement);
+  const missingElement = newArr;
+  // console.log(missingElement); // missing element
+  const arrayToString = numbersProgression.toString();
+  const question = (`${arrayToString}`);
+
+  const correctAnswer = missingElement.toString();
+
+  return [question, correctAnswer];
 };
 
-export default startBrainGame;
+const brainProgressionGame = () => startBrainGame(rulesOfGame, gameCheck);
+
+export default brainProgressionGame;
