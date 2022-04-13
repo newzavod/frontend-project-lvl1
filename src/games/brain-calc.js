ba32d2a336d2ad95427c7654c1c565d2a84b1d89
Vladimir <newzavod@gmail.com>
2022-04-13 08:35:54 +0300

    fix index.js, brain-calc.js need help!

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -4,27 +4,41 @@ import startBrainGame from '../index.js';
 const rulesOfGame = 'What is the result of the expression?.';
 const listOperators = ['+', '-', '*'];
 
+// const correctAnswer = (num1, operator, num2) => {
+//   switch (operator) {
+//     case '+':
+//       return num1 + num2;
+//     case '-':
+//       return num1 - num2;
+//     case '*':
+//       return num1 * num2;
+//     default:
+//       return `Unsupported operator - ${operator}`;
+//   }
+// };
+// console.log(correctAnswer(42, '+', 42)); → 84
+
 const generateRound = () => {
   const randomNumberFirst = _.random(1, 100);
   const randomNumberSecond = _.random(1, 100);
-  // const listOperators = ['+', '-', '*'];
   const randomOperator = listOperators[_.random(listOperators.length - 1)];
   const question = (`${randomNumberFirst} ${randomOperator} ${randomNumberSecond}`);
-  const sum = (randomNumberFirst + randomNumberSecond);
-  const sub = (randomNumberFirst - randomNumberSecond);
-  const mult = (randomNumberFirst * randomNumberSecond);
+
+  // correctAnswer(randomNumberFirst, randomOperator, randomNumberSecond);
+
   const correctAnswer = () => {
     if (randomOperator === '+') {
-      return sum;
+      return (randomNumberFirst + randomNumberSecond);
     }
     if (randomOperator === '-') {
-      return sub;
+      return (randomNumberFirst - randomNumberSecond);
     }
     if (randomOperator === '*') {
-      return mult;
+      return (randomNumberFirst * randomNumberSecond);
     }
-    return [sum, sub, mult];
+    return correctAnswer;
   };
+
   return [question, correctAnswer().toString()];
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 const rounds = 3;
 
 const startBrainGame = (rulesOfGame, generateRound) => {
-  // const rounds = 3;
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
