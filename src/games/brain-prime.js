00290d2f871df36e508ebac0c9efdd844d480f77
Vladimir <newzavod@gmail.com>
2022-04-18 16:28:38 +0300

    changed brain-ptime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,20 +5,20 @@ const rulesOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (number) => {
   if (number < 2) {
-    return 'no';
+    return false;
   }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
-  } return 'yes';
+  } return true;
 };
 
 const generateRound = () => {
   const randomNumber = _.random(1, 100);
   const question = `${randomNumber}`;
 
-  const correctAnswer = String(isPrime(randomNumber));
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
